8bd10e0ac52dc8fc98b5b056c15a538cf3d5ba50
mwendamwenda <benmwenda83@gmail.com>
2022-11-03 12:28:48 +0300

    final commit on objects

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('tokiti is my lovely wife');
+// // console.log('tokiti is my lovely wife'
+const friends = ['mwenda','tokiti','millan'];
+console.log(friends);
+
+const benArray = [
+    'enginer',
+    friends,
+    2037 - 1992,
+    'kenya'
+
+]
+console.log(benArray);
+
+//objects are defined by key and its value eg;
+
+const benObj = {
+    firstName : 'mwenda',
+    lastName : 'kananu',
+    age : 30 ,
+    tribe : 'meru',
+    occupation : 'engineer',
+    friends
+   
+}
+console.log(benObj);
